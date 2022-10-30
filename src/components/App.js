de4123802b7941efb5fdb7da5b44dc5958033fe9
Abhishek kumar <109082907+Abhishek6388@users.noreply.github.com>
2022-10-30 15:02:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 export default function App() {
  //code here 
- const [click, setClick] = useState(0);
+
  
 
  
@@ -12,11 +12,12 @@ export default function App() {
     <div>
    {/* the <p> tag with id text will show the concatinated string as output on string 
       the <input> tag with id input will take input entered by user */}
-  <input id='text' value={inputValue} onChange={changeInput} />
+  <input id='input' value={inputValue} onChange={changeInput} />
   
       <p id='intro'>Concated String</p>
-      <p> you clicked {click} times </p>
-      <button onClick = {() => setClick(click + 1)
+  
+   <p> id ='text'>{text} </p>
+      <button id = button' onClick={buttonClick}>
         Click me
       </button>
     </div>
